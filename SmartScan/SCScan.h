a8da2c04b59a91d66ecdd3b82ec5befdae9ec0ca
Murvin Bhantooa <m.bhantooa@btinternet.com>
2014-04-09 16:54:28 +0100

    Added scan object model.

---
+++ SmartScan/SCScan.h
@@ -0,0 +1,15 @@
+//
+//  SCScan.h
+//  SmartScan
+//
+//  Created by Murvin Bhantooa (O581573) on 09/04/2014.
+//  Copyright (c) 2014 Murvin Bhantooa. All rights reserved.
+//
+
+@import Foundation;
+
+@interface SCScan : NSObject
+
+@property(nonatomic, readonly, strong) NSString *rawPayload;
+
+@end
---
+++ SmartScan/SCScan.m
@@ -0,0 +1,13 @@
+//
+//  SCScan.m
+//  SmartScan
+//
+//  Created by Murvin Bhantooa (O581573) on 09/04/2014.
+//  Copyright (c) 2014 Murvin Bhantooa. All rights reserved.
+//
+
+#import "SCScan.h"
+
+@implementation SCScan
+
+@end
